699833e1a62ad8be20ac47c41f2eea935f430f87
Casa <alonpahe2@gmail.com>
2021-05-26 21:45:14 +0200

    Commit Funcion simplificar

--- src/main/java/com/ejercicioDelCurso/proyectoDePartidaFracciones/Fraccion.java
+++ src/main/java/com/ejercicioDelCurso/proyectoDePartidaFracciones/Fraccion.java
@@ -16,10 +16,27 @@ public class Fraccion {
  * 	Añadir las siguientes operaciones a Fracción
  * 
  * 
- * 
+ */
 	public static Fraccion simplifica(Fraccion entrada) {
+		int num2= entrada.getDenominador();
+		int num1= entrada.getNumerador();
+
+		int temp1=num1;
+		int temp2=num2;
+
+		while (num1!=num2){
+			if (num1> num2){
+				num1=num1-num2;
+			}else {
+				num2=num2-num1;
+			}
+		}
+		int num3= temp1/num1;
+		int num4= temp2/num2;
+
+		return new Fraccion(num3,num4);
 	}
-	
+	/*
 	public static Fraccion suma(Fraccion sumando1, Fraccion sumando2) {
 	}
 	public static Fraccion multiplicacion(Fraccion sumando1, Fraccion sumando2) {
@@ -46,9 +63,7 @@ public class Fraccion {
 
 	@Override
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(numerador).append(" / ").append(denominador);
-		return sb.toString();
+		return numerador + " / " + denominador;
 	}
 
 }
